ed5156d6514e87b5a5b3503275a27ef1fa81aa66
Airon <airon@tark.pro>
2022-06-13 17:04:14 +0300

    example.dart: minor refactor

--- example/example.dart
+++ example/example.dart
@@ -9,7 +9,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        //
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
